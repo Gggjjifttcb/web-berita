eb055ae239324426effde96866b0185ae01e7ef0
unknown <myogaapriadi43@gmail.com>
2025-12-24 10:27:39 +0800

    24 desember

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,16 +10,28 @@
 <div class="login-box">
     <h2>Register Admin</h2>
 
+    @if (session('success'))
+        <div class="alert success">
+            {{ session('success') }}
+        </div>
+    @endif
+
+    @if ($errors->any())
+        <div class="alert error">
+            {{ $errors->first() }}
+        </div>
+    @endif
+
     <form method="POST" action="/register" autocomplete="off">
         @csrf
 
         <input type="text" style="display:none">
         <input type="password" style="display:none">
 
-        <input type="text" name="name_register" placeholder="Nama Lengkap" autocomplete="new-name" required>
-        <input type="email" name="email_register" placeholder="Email" autocomplete="new-email" required>
-        <input type="password" name="password_register" placeholder="Password" autocomplete="new-password" required>
-        <input type="password" name="password_confirm_register" placeholder="Konfirmasi Password" autocomplete="new-password" required>
+        <input type="text" name="name_register" placeholder="Nama Lengkap" required>
+        <input type="email" name="email_register" placeholder="Email" required>
+        <input type="password" name="password_register" placeholder="Password" required>
+        <input type="password" name="password_confirm_register" placeholder="Konfirmasi Password" required>
 
         <button type="submit">Register</button>
     </form>
